d1c80c6a3efbeacc3883b73bcada4530d3405436
Mister-Anonymouse <adityaadi111203@gmail.com>
2025-08-03 17:41:04 +0530

    Game win scene

--- LoopByDeath/Assets/Scripts/LoopManager.cs
+++ LoopByDeath/Assets/Scripts/LoopManager.cs
@@ -16,6 +16,9 @@ public class LoopManager : MonoBehaviour
     public AudioClip gameOverClip;
     public AudioSource bgMusic;
 
+    [Header("Win")]
+    public GameObject winPanel;
+
     [Header("Die button")]
     public GameObject player;
     public Transform respawnPt;
@@ -35,7 +38,7 @@ public class LoopManager : MonoBehaviour
             bgMusic.Pause();
         }
 
-        if (Input.GetButtonDown("E"))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             DieButton();
         }
